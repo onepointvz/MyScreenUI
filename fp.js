a0e3382b4c3993f0722f94b31756bbbf28f6f29a
onepointvz <vzwallet@gmail.com>
2016-02-29 13:41:57 +0530

    Added files via upload

---
+++ fp.js
@@ -0,0 +1,22 @@
+<meta name="author" content="Verizon">
+<meta name="description" content="Denied Access Policy">
+</head>
+<body>
+<font face="Helvetica"><big><bold>Access Denied (content_filter_denied)</bold></font></big>
+<p>
+<font face="Helvetica"><medium>You are attempting to access a prohibited site from the Verizon internal network because of its content categorization: <b>"Social Networking"</b></font></medium>
+<p>
+<font face="Helvetica"><medium>This activity is being blocked by Verizon and a record of this site access has been recorded.</font></medium>
+<p>
+<font face="Helvetica"><medium>Per CPI-810 and the Verizon Code of Conduct, all company systems are to be used for company purposes.  Violation of company policy could result in disciplinary action, up to and including termination.</font></medium>
+<p>
+<font face="Helvetica"><medium><b>If your job function requires access to this site, an exception request can be submitted via the ACSR system</b></font></medium>
+<p>
+<h3><a href="https://acsr.tdc.vzwcorp.com/ACSR/index">https://acsr.tdc.vzwcorp.com/ACSR/index</a></h3></font></medium>
+<p>
+<font face="Helvetica"><medium>Select <b>Submit ACSR</b>.  Enter <b>Internet Extended Access</b> in the Form Search box, scroll down, select the form and click NEXT.
+               Complete the ACSR form including a valid business justification for extended access. Your access request will be reviewed by Security.  You will receive an email from the ACSR system upon completion of your request.</font></medium>
+<p>
+<font face="Helvetica"><small><i>Support information / SG:irv-bcoat-2 / [29/02/2016:08:10:14 GMT]</font></i></small>
+</body>
+</html>
